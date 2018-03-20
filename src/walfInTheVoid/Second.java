100414e251b63ce18603b21baaa298d6f869b07a
Benja <Benjalomen@users.noreply.github.com>
2018-03-20 16:31:43 -0400

    Updating To Do's
    Comments more than anything

--- src/walfInTheVoid/Second.java
+++ src/walfInTheVoid/Second.java
@@ -1,10 +1,12 @@
 package walfInTheVoid;
 //TO DO:
-//- Explain experience from 3rd person limited POV
+//- Still trying to figure out what POV --> maybe 3rd person limited
 //- if health <= 0 --> return to ship with Ship()
 //- if pluck <= 0 --->  ???????
+//- if focus <= 0 ---> ???????
 public class Second {
 	
+	//Insert name of your planet!
 	static String planetName = "INSERT NAME HERE!!!";
 
 	// Your adventure should be written below
